1db5efc72eaaa2f4bdcb923c82296592a0b3e99d
Dmitriy Grigorev <dimag@inbox.ru>
2022-08-11 15:27:52 +0300

    Remove unused link

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     # path('admin/action/create-tables-action/<int:object_pk>',
     #      views.create_import_tables_action, name='import-tables-action'),
-    re_path(r'^admin/action/create-tables-list/(?P<object_pk>\d+)/$',
-        views.create_or_update_import_tables_list, name='import-tables-action'),
+    # re_path(r'^admin/action/create-tables-list/(?P<object_pk>\d+)/$',
+    #     views.create_or_update_import_tables_list, name='import-tables-action'),
     re_path(r'^admin/action/run-import-for-single-table/(?P<table_pk>\d+)/$',
             views.run_import_for_single_table, name='run-import-for-single-table'),
     re_path(r'^admin/action/run-import-for-database/(?P<poll_pk>\d+)/$',
