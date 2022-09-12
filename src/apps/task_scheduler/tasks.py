2c184aa922a466e831dd829fe8b6ac50af572c32
Dmitriy Grigorev <dimag@inbox.ru>
2022-09-12 13:49:17 +0300

    Fix error in select_tables_for_imports

--- src/apps/task_scheduler/tasks.py
+++ src/apps/task_scheduler/tasks.py
@@ -1,8 +1,6 @@
 import logging
-# import time
 import dramatiq
 from src.apps.task_scheduler.cron import cron
-from src.apps.core.managers import ConnectSetManager
 from src.apps.core.tasks import process_database_import
 from src.apps.core.functions import tables_import_info_list
 
@@ -11,32 +9,16 @@ from src.apps.core.functions import tables_import_info_list
 #     level=logging.DEBUG,
 # )
 
-# Pika is a bit noisy w/ Debug logging so we have to up its level.
 logger = logging.getLogger(__name__)
-
-# @cron("* * * * *")  # Run once a minute
-# @dramatiq.actor()
-# def print_current_datetime():
-#     logging.warning('Start my long-running task')
-#     time.sleep(5)
-#     logging.warning(f'Last write time is: {datetime.now()}')
-
-
-# @cron("* * * * *")  # Run once a minute
-# @dramatiq.actor()
-# def process_user_stats():
-#     """Very simple task for demonstrating purpose."""
-#     logging.warning('Start my long-running task')
-#     time.sleep(5)
-#     logging.warning('Task is ended')
 
 @cron("*/20 * * * *")  # Run once 20 minutes
 @dramatiq.actor()
 def select_tables_for_imports():
     """Just do reimport it data from DBF tables if they have  changed last write date time"""
     # https://linuxize.com/post/cron-jobs-every-5-10-15-minutes/
+    from src.apps.core.models import ConnectSet
 
-    data_polls = ConnectSetManager.allowed_for_import()
+    data_polls = ConnectSet.consets.allowed_for_import()
     for poll in data_polls:
         params = tables_import_info_list(poll.pk)
         if len(params) > 0:
