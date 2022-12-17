eed6754af32a41b2391275e6eebc1dc81b6437c7
Dmitriy Grigorev <dimag@inbox.ru>
2022-12-17 22:11:29 +0300

    chore: Add support static and media in production

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -1,17 +1,13 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as mediaserve
 from src.apps.common.dataclasses import ETL
+
 
 from . import views
 
 urlpatterns = [
-    # path('admin/action/create-tables-action/<int:object_pk>',
-    #      views.create_import_tables_action, name='import-tables-action'),
-    # re_path(r'^admin/action/create-tables-list/(?P<object_pk>\d+)/$',
-    #     views.create_or_update_import_tables_list, name='import-tables-action'),
-    # re_path(r'^admin/action/' + f'{ETL.URLNAME.EXPORT_SINGLE_TABLE}' + '/(?P<table_pk>\d+)/$',
-    #         views.run_import_for_single_table, name=f'{ETL.URLNAME.EXPORT_SINGLE_TABLE}'),
     path('admin/action/' + f'{ETL.URLNAME.EXPORT_SINGLE_TABLE}' + '/<int:table_pk>/<str:mode>/',
             views.run_import_for_single_table, name=f'{ETL.URLNAME.EXPORT_SINGLE_TABLE}'),
     # re_path(r'^admin/action/' + f'{ETL.URLNAME.PIPELINE_EXPORT_IMPORT}' + '/(?P<poll_pk>\d+)/$',
@@ -20,11 +16,6 @@ urlpatterns = [
             views.run_import_for_database, name=f'{ETL.URLNAME.PIPELINE_EXPORT_IMPORT}'),
     # path('admin/action/run-import-for-database/<int:table_pk>/<str:mode>/',
     #         views.run_import_for_database, name='run-import-for-database'),
-
-    # path("sqlimport/", include("src.apps.sqlimport.urls")),
-
-    # path('export/category/', views.CategoryExportView.as_view(), name='export-category'),
-
 ]
 
 if settings.DEBUG:
@@ -32,6 +23,13 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
-
-if settings.MEDIA_ROOT:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+                mediaserve, {'document_root': settings.MEDIA_ROOT}
+        ),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+                mediaserve, {'document_root': settings.STATIC_ROOT}
+                ),
+    ]
