d771bcfc40c5f9888063fef30675a436f83f52ed
Dmitriy Grigorev <dimag@inbox.ru>
2022-12-20 12:52:45 +0300

    chore: Add support with static files

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -1,7 +1,8 @@
+import re
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve as mediaserve
+# from django.views.static import serve as mediaserve
 from src.apps.common.dataclasses import ETL
 
 
@@ -23,13 +24,26 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += [
-        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
-                mediaserve, {'document_root': settings.MEDIA_ROOT}
-        ),
-        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
-                mediaserve, {'document_root': settings.STATIC_ROOT}
-                ),
-    ]
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# else:
+#     urlpatterns += [
+#         re_path(
+#             r"^%s(?P<path>.*)$" % re.escape(settings.STATIC_URL.lstrip("/")),
+#             mediaserve, {'document_root': settings.STATIC_ROOT}
+#         ),
+#     ]
+    # thing = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # print(thing, type(thing[0]))
+    # print(thing[0].pattern)
+    # print(dir(thing[0]))
+    # print(urlpatterns[1], type(urlpatterns[1]))
+
+    # urlpatterns += [
+    #     re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+    #             mediaserve, {'document_root': settings.MEDIA_ROOT}
+    #     ),
+    #     re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+    #             mediaserve, {'document_root': settings.STATIC_ROOT}
+    #             ),
+    # ]
