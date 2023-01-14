a038bf82780cba789b5151aff107c269e2c663e2
Dmitriy Grigorev <dimag@inbox.ru>
2023-01-14 21:36:32 +0300

    chore: Add email.py

--- src/config/email.py
+++ src/config/email.py
@@ -1,11 +1,24 @@
 from src.config.env import env
 
-# Email settings
+# https://django-environ.readthedocs.io/en/latest/tips.html#email-settings
+# DJANGO_ADMINS=Blake:blake@cyb.org,Alice:alice@cyb.org
+ADMINS = [x.split(':') for x in env.list('DJANGO_ADMINS')]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = env('EMAIL_HOST', default='mx1.ntbroker.ru')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='exchange')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default=None)
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 # Custom setting. To email
-RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
+# RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
+
+# from django.core.mail import send_mail
+# send_mail(
+#     subject='A cool subject',
+#     message='A stunning message',
+#     from_email='exchange@ntbroker.ru',
+#     recipient_list=ADMINS,
+#     auth_user=EMAIL_HOST_USER,
+#     auth_password=EMAIL_HOST_PASSWORD
+# )
